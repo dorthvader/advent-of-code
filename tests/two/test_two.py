9b0c45211b08b3fd1f08b22b021ebf8d315ca4c2
Dorothy House <dorothydavishouse@gmail.com>
2023-12-03 10:35:19 -0700

    test(two): add tests for day two

---
+++ tests/two/test_two.py
@@ -0,0 +1,42 @@
+from advent_of_code.current.two import parse_id, parse_turn, parse_game, Game
+from advent_of_code.current.two_main import get_total
+
+import pathlib
+
+ROW = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
+
+DATA_PATH = pathlib.Path.cwd() / "tests/two/sample_inputs/1.txt"
+
+def test_parse_id():
+    id, rest_of_str = parse_id(ROW)
+    assert "1" == id
+    assert " 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green" == rest_of_str
+
+def test_turn():
+    turn = "3 blue, 4 red, 1 green"
+    out = parse_turn(turn)
+
+    expected = {
+        "red": 4,
+        "green": 1,
+        "blue": 3
+    }
+
+    assert out == expected
+
+
+def test_parse_game():
+    g = parse_game(ROW)
+
+    assert isinstance(g, Game)
+    assert g.colors["blue"] == 6
+    assert g.colors["red"] == 4
+    assert g.colors["green"] == 2
+
+def test_get_total():
+    exp_total = 8
+
+    total = get_total(file_path=DATA_PATH)
+
+    assert total == exp_total
+
